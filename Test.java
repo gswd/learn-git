2519fe52b50c7495a7dca6f71c40920020929f93
lihao <lihao411@163.com>
2017-07-09 12:57:11 +0800

    modify Test.java

--- Test.java
+++ Test.java
@@ -1,35 +1,70 @@
 public class Test{
 	
-	
 	public static void main(String[] args){
-		//Person p = new Person();
-		Student s = new Student("123");
+		Student s = new Student("cat", "12");
+		
+		Student s1 = new Student("cat", "12");
+		
+		System.out.print(s.equals(s1));
+		
+		
+		
 	}
 }
 
-class Person{
-	
+
+class Student{
 	private String name;
+	private String age;
 	
-	public Person(String name){
-		System.out.println("person constructor [" + name + "] ...");
+	public Student(String name, String age){
+
 		this.name = name;
+		this.age = age;
+
 	}
 	
+	public Student(){
+		
+	}
 	
+	public String getName(){
+		return this.name;
+	}
 	
-	public String info(){
-		return null;
+	public String getAge(){
+		return this.age;
+	}
+	public String toString(){
+		return "name:" + name + " age:" + age;
+	}
+	
+	public boolean equals(Object obj){
+		if(obj == null){
+			return false;
+		}
+		
+		if(!(obj instanceof Student)){
+			return false;
+			
+		}
+		
+		Student s = (Student)obj;
+		
+		if(!this.name.equals(s.getName())){
+			return false;
+		}
+		
+		if(!this.age.equals(s.getAge())){
+			return false;
+		}
+		
+		return true;
+		
+		
 	}
 }
 
-class Student extends Person {
-	
-	private String school;
-	
-	public Student(String school){
-		super();
-		System.out.println("Student constructor ...");
-		this.school = school;
-	}
-}
+
+//Student@SDF#FwSD
+//name:cat age:12
